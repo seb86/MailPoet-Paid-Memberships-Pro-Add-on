32de213d5bbfc279114a8395701c657e52f5c51f
seb86 <sebastien@sebs-studio.com>
2014-03-03 20:41:02 +0100

    Updated uninstall

--- uninstall.php
+++ uninstall.php
@@ -15,9 +15,16 @@ if( !defined('WP_UNINSTALL_PLUGIN') ) exit();
 // For Single site
 if ( !is_multisite() ) {
 
-	// Your uninstall code goes here.
-	// List each option to delete here.
-	delete_option( 'option_name' );
+	delete_option( 'mailpoet_paid_memberships_pro_add_on_enable_checkout' );
+	delete_option( 'mailpoet_paid_memberships_pro_add_on_checkout_label' );
+	delete_option( 'mailpoet_paid_memberships_pro_subscribe_too' );
+
+	// Delete user meta data from all users.
+	$all_user_ids = get_users( 'fields=ID' );
+	foreach ( $all_user_ids as $user_id ) {
+		delete_user_meta( $user_id, 'pmpro_user_subscribe_to_mailpoet' );
+	}
+
 } 
 // For Multisite
 else {
@@ -29,9 +36,9 @@ else {
 	foreach ( $blog_ids as $blog_id ) {
 		switch_to_blog( $blog_id );
 
-		// Your uninstall code goes here.
-		// List each option to delete here.
-		delete_site_option( 'option_name' );
+		delete_site_option( 'mailpoet_paid_memberships_pro_add_on_enable_checkout' );
+		delete_site_option( 'mailpoet_paid_memberships_pro_add_on_checkout_label' );
+		delete_site_option( 'mailpoet_paid_memberships_pro_subscribe_too' );
 	}
 
 	switch_to_blog( $original_blog_id );
