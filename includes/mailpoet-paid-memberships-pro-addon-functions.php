d612cd04ba52b1b836a287ecc6616059671f7a68
greathmaster <counthmaster@yahoo.com>
2017-07-10 16:02:02 -0700

    BUG: Fixed required fields error when user was already logged in

--- includes/mailpoet-paid-memberships-pro-addon-functions.php
+++ includes/mailpoet-paid-memberships-pro-addon-functions.php
@@ -42,7 +42,10 @@ function mailpoet_pmpro_registration_checks($okay) {
 	if( !empty( $_REQUEST['pmpro_user_subscribe_to_mailpoet'] ) ) {
 		global $pmpro_msg, $pmpro_msgt, $pmpro_error_fields;
 
-		$required_fields = array('username', 'bemail');
+		if(!is_user_logged_in())
+			$required_fields = array('username', 'bemail');
+		else
+			$required_fields = array();
 
 		foreach($required_fields as $field) {
 			if(empty($_REQUEST[$field])){
